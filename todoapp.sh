43dc49a6279cd7388eb182918e51966f66cbabc4
Thomas <thomas40651@gmail.com>
2022-04-23 21:46:06 -0300

    changing the exec

--- todoapp.sh
+++ todoapp.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-chmod 755 todoapp.sh
 
+chmod 755 todoapp.sh
+cd to_doAPI
+cd todofront
 
 sudo apt-get update
 
-sudo apt-get install mysql-apt-config
-sudo snap install mysql-shell
 
 sudo apt install nodejs
 sudo apt install npm
 
-
+npm install
 
 npm start
+
